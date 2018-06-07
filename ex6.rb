81f5a3ffba671da6254a58f1f7ea4f437f9f647f
dougsmock <39506285+dougsmock@users.noreply.github.com>
2018-06-06 20:19:54 -0400

    Add files via upload

---
+++ ex6.rb
@@ -0,0 +1,21 @@
+typesOfPeople = 10
+x = "There are #{typesOfPeople} types of people."
+binary = "binary"
+doNot = "don't"
+y = "Those who know #{binary} and those who #{doNot}."
+
+puts x
+puts y
+
+puts "I said #{x}."
+puts "I also said #{y}."
+
+hilarious = false
+jokeEvaluation = "Isn't that joke funny?! #{hilarious}"
+
+puts jokeEvaluation
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+puts w + e
