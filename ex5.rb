883d1843c783a24a7d964245df4019fe2c573c92
dougsmock <39506285+dougsmock@users.noreply.github.com>
2018-06-06 20:15:50 -0400

    Add files via upload

---
+++ ex5.rb
@@ -0,0 +1,18 @@
+name = 'Doug Smock'
+age = 54
+height = 76
+weight = 250
+eyes = 'Green'
+teeth = 'White'
+hair = 'Black'
+heightCm = height * 2.54
+weightKg = weight * 0.453592
+
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall, #{heightCm} centimeters."
+puts "He's #{weight} pounds heavy, #{weightKg} kilograms."
+puts "Actually, that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+
+puts "If I add #{age}, #{height} and #{weight}, I get #{age + height + weight}."
