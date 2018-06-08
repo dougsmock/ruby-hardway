fa78ea77024079d9b58fb92cff52289d27d95045
dougsmock <39506285+dougsmock@users.noreply.github.com>
2018-06-08 10:51:52 -0400

    Add files via upload

---
+++ ex10.rb
@@ -0,0 +1,15 @@
+tabbyCat = "\tI'm tabbed in."
+persianCat = "I'm split\non a line."
+backslashCat = "I'm \\ a \\ cat."
+
+fatCat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+puts tabbyCat
+puts persianCat
+puts backslashCat
+puts fatCat
