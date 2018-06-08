280120267c59ad7b19b81ee390dd11fd1f4cc468
dougsmock <39506285+dougsmock@users.noreply.github.com>
2018-06-08 11:23:36 -0400

    Add files via upload

---
+++ ex13.rb
@@ -0,0 +1,6 @@
+first, second, third, something = ARGV
+
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is #{third}"
+puts "Your fourth variable is #{something}"
